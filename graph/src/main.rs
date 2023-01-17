3129c58370c329998290a0d08a716d9b77bd8e3e
Kamil <kamil.figon@gmail.com>
2023-01-17 21:46:27 +0100

    first commit

---
+++ graph/src/main.rs
@@ -0,0 +1,52 @@
+use std::collections::{HashMap, HashSet};
+
+fn main() {
+    println!("Hello, world!");
+    let data = vec![
+        ("A","B"),
+        ("A","C"),
+        ("C","D"),
+        ("E","F"),
+    ];
+    let mut g = Graph::new();
+    data.iter().for_each(|d| g.connect(d.0, d.1));
+    println!("{:?}", g);
+    println!("{:?}", g.collect())
+}
+
+#[derive(Debug)]
+struct Graph(HashMap<String, HashSet<String>>);
+
+impl Graph {
+    fn new() -> Graph {
+        Graph(HashMap::new())
+    }
+
+    fn connect(&mut self, a: &str, b: &str) {
+        self.0.entry(a.to_string())
+            .and_modify(|s| { s.insert(b.to_string()); })
+            .or_insert(HashSet::from([b.to_string()]));
+
+        self.0.entry(b.to_string())
+            .and_modify(|s| { s.insert(a.to_string()); })
+            .or_insert(HashSet::from([a.to_string()]));
+    }
+
+    fn collect(self) -> Vec<String> {
+        let mut visited = HashSet::<String>::new();
+        fn dfs(graph: &Graph, visited: &mut HashSet<String>, node: &String) {
+            if visited.contains(node) {
+                return;
+            }
+            visited.insert(node.clone());
+            let children = match graph.0.get(node) {
+                Some(v) => v,
+                None => return,
+            };
+            children.iter().for_each(|n| dfs(graph, visited, n));
+        }
+
+        self.0.keys().for_each(|k| dfs(&self, &mut visited, k));
+        visited.into_iter().collect()
+    }
+}
---
+++ simple-lexer/src/main.rs
@@ -0,0 +1,104 @@
+use std::{iter::Peekable, str::Chars};
+
+fn main() {
+    println!("Hello, world!");
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::*;
+
+    #[test]
+    fn lex1() {
+        let input = "let xd = 15 + 1;";
+        let expected = vec![
+            Token::Keyword("let".to_string()),
+            Token::Identifier("xd".to_string()),
+            Token::Operator("=".to_string()),
+            Token::Number("15".to_string()),
+            Token::Operator("+".to_string()),
+            Token::Number("1".to_string()),
+            Token::Semicolon(";".to_string()),
+        ];
+        assert_eq!(lexer(input), expected);
+    }
+    
+    #[test]
+    fn lex2() {
+        let input = "let xd=15 ==31);";
+        let expected = vec![
+            Token::Keyword("let".to_string()),
+            Token::Identifier("xd".to_string()),
+            Token::Operator("=".to_string()),
+            Token::Number("15".to_string()),
+            Token::Operator("==".to_string()),
+            Token::Number("31".to_string()),
+            Token::Closing(")".to_string()),
+            Token::Semicolon(";".to_string()),
+        ];
+        assert_eq!(lexer(input), expected);
+    }
+}
+
+#[derive(PartialEq, Debug)]
+enum Token {
+    Opening(String),
+    Closing(String),
+    Operator(String),
+    Keyword(String),
+    Identifier(String),
+    Number(String),
+    Semicolon(String),
+}
+
+fn lexer(input: &str) -> Vec<Token> {
+    let mut out = Vec::<Token>::new();
+    let mut chars = input.chars().peekable();
+    let is_keyword = |word: &String| word == "if" || word == "for" || word == "let";
+
+    while let Some(current_char) = chars.next() {
+        let next = chars.peek();
+
+        if current_char.is_whitespace() {
+            continue;
+        } else if current_char == ';' {
+            out.push(Token::Semicolon(current_char.to_string()));
+        }else if current_char == '(' || current_char == '{' {
+            out.push(Token::Opening(current_char.to_string()));
+        } else if current_char == ')' || current_char == '}' {
+            out.push(Token::Closing(current_char.to_string()));
+        } else if current_char == '+' || current_char == '-' || current_char == '/' || current_char == '*' {
+            out.push(Token::Operator(current_char.to_string()));
+        } else if current_char == '=' {
+            let word = read_until(&mut chars, current_char,|c| c == '=');
+            out.push(Token::Operator(word)); // == or =
+        } else if current_char.is_ascii_digit() {
+            let num = read_until(&mut chars, current_char,|c| c.is_ascii_digit());
+            out.push(Token::Number(num));
+        } else {
+            let word = read_until(&mut chars, current_char,|c| c.is_alphabetic());
+            if is_keyword(&word) {
+                out.push(Token::Keyword(word));
+            } else {
+                out.push(Token::Identifier(word));
+            }
+        }
+    }
+
+    out
+}
+
+fn read_until<F>(chars: &mut Peekable<Chars>, current_char: char, fun: F) -> String 
+    where F: Fn(char) -> bool {
+    let mut out = String::new();
+    out.push(current_char);
+
+    while let Some(c) = chars.peek() {
+        if fun(*c) {
+            out.push(chars.next().unwrap());
+        } else {
+            break;
+        }
+    }
+    out
+}
