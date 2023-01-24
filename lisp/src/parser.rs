dd38f4e99b07bf7d13acc168a6cd49a9bde10d0c
Kamil <kamil.figon@gmail.com>
2023-01-24 20:54:44 +0100

    more stuff in parser

--- lisp/src/parser.rs
+++ lisp/src/parser.rs
@@ -1,45 +1,75 @@
-use crate::lexer::{lex, Token};
+use std::{iter::Peekable, slice::Iter};
+
+use crate::lexer::{Token};
 
 #[derive(Debug, PartialEq)]
-pub struct Ast;
+pub struct Ast(Vec<SExpression>);
+
+#[derive(Debug, PartialEq)]
+pub enum SExpression {
+}
 
 #[derive(Debug, PartialEq)]
 pub enum CompilerError {
     InvalidToken(String),
 }
 
-pub fn parse(tkns: Vec<Token>) -> Result<Ast, CompilerError> {
-    let mut tokens = tkns.iter().peekable();
+struct Parser {
+    errors: Vec<CompilerError>,
+    expressions: Vec<SExpression>,
+    tokens: Vec<Token>,
+}
 
-    while let Some(current) = tokens.next() {
-        let next = tokens.peek();
-
-        match current {
-            Token::Invalid(line, t) => {
-                return Err(CompilerError::InvalidToken(format!(
-                    "Invalid token on {line}: {}",
-                    t.to_owned()
-                )))
-            }
-            Token::Opening(_) => todo!(),
-            Token::Closing(_) => todo!(),
-            Token::Operator(_) => todo!(),
-            Token::Number(_) => todo!(),
-            Token::Keyword(_) => todo!(),
-            Token::Identifier(_) => todo!(),
-            Token::String(_) => todo!(),
-            Token::Boolean(_) => todo!(),
+impl Parser {
+    fn new(tokens: Vec<Token>) -> Parser {
+        Parser { errors: vec![],
+             expressions: vec![],
+             tokens: tokens,
         }
     }
 
-    Ok(Ast)
+    fn run(&mut self) {
+        let mut tokens  = self.tokens.iter_mut().peekable();
+
+        while let Some(current) = tokens.next() {
+            let next = tokens.peek();
+            
+            match current {
+                Token::Invalid(line, t) => {
+                    self.errors.push(
+                        CompilerError::InvalidToken(
+                            format!("Invalid token on {line}: {}", t.to_owned())
+                        ));
+                    break;
+                }
+                Token::Opening(_) => todo!(),
+                Token::Closing(_) => todo!(),
+                Token::Operator(_) => todo!(),
+                Token::Number(_) => todo!(),
+                Token::Keyword(_) => todo!(),
+                Token::Identifier(_) => todo!(),
+                Token::String(_) => todo!(),
+                Token::Boolean(_) => todo!(),
+            }
+        }
+    }
+}
+
+pub fn parse(tokens: Vec<Token>) -> Result<Ast, Vec<CompilerError>> {
+    let mut p = Parser::new(tokens);
+    p.run();
+    if p.errors.len() != 0 {
+        return Err(p.errors);
+    }
+    Ok(Ast(p.expressions))
 }
 
 #[cfg(test)]
 mod tests {
+    use crate::lexer::lex;
     use super::*;
 
-    fn compile(input: &str) -> Result<Ast, CompilerError> {
+    fn compile(input: &str) -> Result<Ast, Vec<CompilerError>> {
         parse(lex(input))
     }
 
@@ -48,7 +78,17 @@ mod tests {
         let tok = "(define x \"invalid string";
         assert_eq!(
             compile(tok),
-            Err(CompilerError::InvalidToken("\"invalid string".to_string()))
+            Err(vec![CompilerError::InvalidToken("\"invalid string".to_string())])
+        )
+    }
+
+    #[test]
+    fn parse_simple_math() {
+        todo!();
+        let tok = "(+ 3 1)";
+        assert_eq!(
+            compile(tok),
+            Ok(Ast(vec![]))
         )
     }
 }
