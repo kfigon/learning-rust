229b3177e283cde1d3874492f8a826ea509cd7d0
kfigon <kamil.figon@gmail.com>
2023-08-16 18:49:55 +0200

    iterators fun

--- playground/src/iterators.rs
+++ playground/src/iterators.rs
@@ -1,4 +1,4 @@
-use std::iter::Iterator;
+use std::{iter::Iterator, collections::HashMap};
 
 struct LimitedIterator{
     limit: i32,
@@ -73,4 +73,43 @@ fn wrapping_iter() {
     assert_eq!(Some(1), i.next());
     assert_eq!(Some(2), i.next());
     assert_eq!(Some(3), i.next());
+}
+
+// will work also with std::env::args()
+fn collect_args(v: impl Iterator<Item = String>) -> HashMap<String, String> {
+    v.skip(1)
+    .collect::<Vec<String>>()
+    .chunks(2)
+    .filter_map(|pair| match pair {
+        [key,val] => Some((key.to_owned(), val.to_owned())),
+        _ => None
+    })
+    .collect::<HashMap<String,String>>()
+}
+
+#[test]
+fn collect_args_empty() {
+    let out = collect_args(Vec::<String>::new().into_iter());
+
+    assert_eq!(out, HashMap::new());
+}
+
+#[test]
+fn collect_args_pairs() {
+    let out = collect_args(vec!["the path", "foo", "bar", "asd", "123"].iter().map(|v|v.to_string()));
+
+    assert_eq!(out, HashMap::from_iter(vec![
+        ("foo".to_string(), "bar".to_string()),
+        ("asd".to_string(), "123".to_string()),
+    ]));
+}
+
+#[test]
+fn collect_args_pairs_with_redundant_key() {
+    let out = collect_args(vec!["the path", "foo", "bar", "asd", "123", "skip meh"].iter().map(|v|v.to_string()));
+
+    assert_eq!(out, HashMap::from_iter(vec![
+        ("foo".to_string(), "bar".to_string()),
+        ("asd".to_string(), "123".to_string()),
+    ]));
 }
