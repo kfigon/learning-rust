a67f7d3b1ebdb80a621426b07fa28ff3ad7d7a5f
kfigon <kamil.figon@gmail.com>
2023-08-17 17:31:37 +0200

    collection of trait objects

--- playground/src/traits.rs
+++ playground/src/traits.rs
@@ -6,10 +6,17 @@ mod tests {
     }
 
     struct Data(String);
+    struct Data2(i32);
 
     impl Doer for Data {
         fn do_it(&self) -> String {
             self.0.to_owned()
+        }
+    }
+
+    impl Doer for Data2 {
+        fn do_it(&self) -> String {
+            self.0.to_string()
         }
     }
 
@@ -38,5 +45,29 @@ mod tests {
 
         assert_eq!(dynamic_dispatch(&d), "foobar".to_string());
         assert_eq!(dynamic_dispatch_2(Box::new(d)), "foobar".to_string());
+    }
+
+    #[test]
+    fn trait_collection_test() {
+        let x1 = Data("foo".to_string());
+        let x2 = Data("bar".to_string());
+        let x3 = Data("asd".to_string());
+        let x4 = Data2(4);
+
+        let refs: Vec<&dyn Doer> = vec![&x1, &x2, &x3, &x4];
+
+        assert_eq!(refs.iter().map(|v| v.do_it()).collect::<Vec<String>>(), vec!["foo".to_string(), "bar".to_string(), "asd".to_string(), "4".to_string()]);
+    }
+
+    #[test]
+    fn trait_collection_test2() {
+        let boxed: Vec<Box<dyn Doer>> = vec![
+            Box::new(Data("foo".to_string())),
+            Box::new(Data("bar".to_string())),
+            Box::new(Data("asd".to_string())),
+            Box::new(Data2(4)),
+        ];
+
+        assert_eq!(boxed.iter().map(|v| v.do_it()).collect::<Vec<String>>(), vec!["foo".to_string(), "bar".to_string(), "asd".to_string(), "4".to_string()]);
     }
 }
