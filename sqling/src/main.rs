56911ec973f037aef7edc024fd885ad96a6e8428
kfigon <kamil.figon@gmail.com>
2023-08-19 12:53:20 +0200

    basic sqlite

--- sqling/src/main.rs
+++ sqling/src/main.rs
@@ -1,4 +1,4 @@
-use rusqlite::{Connection, Result, Params, Error};
+use rusqlite::Connection;
 
 #[derive(Debug, PartialEq, Eq)]
 struct Person {
@@ -7,43 +7,15 @@ struct Person {
     data: Option<Vec<u8>>,
 }
 
-struct Dao {
-    conn: Connection
-}
-
-impl Dao {
-    fn new(conn: Connection) -> Self {
-        Self { conn: conn }
-    }
-
-    fn execute<P: Params>(&self, sql: &str, par: P) -> Result<usize> {
-        self.conn.execute(sql, par)
-    }
-
-    fn query<P: Params>(&self, sql: &str, par: P) -> Result<Vec<Person>, Error> {
-        let mut stmt = self.conn.prepare(sql)?;
-
-        let it = stmt.query_map(par, |row| {
-            Ok(Person {
-                id: row.get(0)?,
-                name: row.get(1)?,
-                data: row.get(2)?,
-            })
-        })?;
-
-        it.collect()
-    }
-}
-
 fn main() {
     println!("hello")
 }
 
 #[test]
 fn dao_test() {
-    let dao = Dao::new(Connection::open_in_memory().unwrap());
+    let conn = Connection::open_in_memory().unwrap();
 
-    dao.execute("CREATE TABLE person (
+    conn.execute("CREATE TABLE person (
             id    INTEGER PRIMARY KEY,
             name  TEXT NOT NULL,
             data  BLOB)", ()).unwrap();
@@ -54,8 +26,19 @@ fn dao_test() {
         data: None,
     };
     
-    dao.execute("INSERT INTO person (name, data) VALUES (?1, ?2)", (&me.name, &me.data)).unwrap();
+    conn.execute("INSERT INTO person (name, data) VALUES (?1, ?2)", (&me.name, &me.data)).unwrap();
 
-    let iter = dao.query("SELECT id, name, data FROM person where name = (?1)", [("Steven".to_owned())]).unwrap();
-    assert_eq!(vec![Person{id: 1, name: "Steven".to_string(), data: None}], iter);
+    let mut statement = conn.prepare("SELECT id, name, data FROM person where name = (?1)").unwrap();
+
+    let data: Vec<Person> = statement.query_map([("Steven".to_owned())], |row| {
+        Ok(Person {
+            id: row.get(0).unwrap(),
+            name: row.get(1).unwrap(),
+            data: row.get(2).unwrap(),
+        })
+    }).unwrap()
+    .filter_map(|v| v.ok())
+    .collect();
+
+    assert_eq!(vec![Person{id: 1, name: "Steven".to_string(), data: None}], data);
 }
