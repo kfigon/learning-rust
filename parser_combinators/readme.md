2a10bf8bfa587aecb6935390159c155138eefb74
kfigon <kamil.figon@gmail.com>
2023-09-21 22:28:21 +0200

    basic parser components

---
+++ parser_combinators/readme.md
@@ -0,0 +1,2 @@
+# parser combinators
+better, higher level, composable "regex"
--- parser_combinators/src/main.rs
+++ parser_combinators/src/main.rs
@@ -10,7 +10,14 @@ enum ParsingErr {
 }
 
 #[derive(Debug, PartialEq)]
-struct ParserResult;
+struct ParserResult{
+    res: String
+}
+impl ParserResult {
+    fn new(res: String) -> Self {
+        Self { res }
+    }
+}
 
 trait Parser {
     fn parse(&self, s: &str) -> Result<ParserResult, ParsingErr>;
@@ -29,13 +36,23 @@ impl<'a> StringParser<'a> {
 impl<'a> Parser for StringParser<'a> {
     fn parse(&self, s: &str) -> Result<ParserResult, ParsingErr> {
         if s.starts_with(self.pattern) {
-            Ok(ParserResult)
+            Ok(ParserResult::new(self.pattern.to_string()))
         } else {
             Err(ParsingErr::NotFound)
         }
     }
 }
 
+struct DigitParser;
+
+impl Parser for DigitParser {
+    fn parse(&self, s: &str) -> Result<ParserResult, ParsingErr> {
+        match s.chars().next() {
+            Some(v) if v.is_digit(10) => Ok(ParserResult::new(v.to_string())),
+            _ => Err(ParsingErr::NotFound),
+        }
+    }
+}
 
 #[cfg(test)]
 mod test {
@@ -46,12 +63,28 @@ mod test {
         let s = StringParser::new("hello");
         assert_eq!(s.parse("foo"), Err(ParsingErr::NotFound));
         assert_eq!(s.parse("hell"), Err(ParsingErr::NotFound));
+        assert_eq!(s.parse(""), Err(ParsingErr::NotFound));
     }
 
     #[test]
     fn str_parser() {
         let s = StringParser::new("hello");
-        assert_eq!(s.parse("hello w"), Ok(ParserResult));
-        assert_eq!(s.parse("hello"), Ok(ParserResult))
+        assert_eq!(s.parse("hello w"), Ok(ParserResult::new("hello".to_string())));
+        assert_eq!(s.parse("hello"), Ok(ParserResult::new("hello".to_string())))
+    }
+
+    #[test]
+    fn digit_parser_invalid() {
+        let d = DigitParser;
+        assert_eq!(d.parse("foo"), Err(ParsingErr::NotFound));
+        assert_eq!(d.parse("hell"), Err(ParsingErr::NotFound));
+        assert_eq!(d.parse(""), Err(ParsingErr::NotFound));
+    }
+
+    #[test]
+    fn digit_parser() {
+        let d = DigitParser;
+        assert_eq!(d.parse("5"),   Ok(ParserResult::new("5".to_string())));
+        assert_eq!(d.parse("123"), Ok(ParserResult::new("1".to_string())));
     }
 }
