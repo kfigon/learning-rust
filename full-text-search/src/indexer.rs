d455cb128200e0b3017db7f0bd92de49b88d1359
kfigon <kamil.figon@gmail.com>
2023-10-20 10:50:40 +0200

    tests moved to indexer

--- full-text-search/src/indexer.rs
+++ full-text-search/src/indexer.rs
@@ -56,3 +56,41 @@ fn tf(input: &str) -> TermFreq {
 
     TermFreq(r)
 }
+
+
+#[test]
+fn smoke_test() {
+    let sentences = HashMap::from_iter([
+        ("A", "The car is driven on the road"),
+        ("B", "The truck is driven on the highway"),
+    ]);
+
+    let res = calc(&sentences);
+    let empty = HashMap::from_iter([
+        ("A", 0.0),
+        ("B", 0.0)
+    ]);
+    assert_eq!(&empty, res.0.get("is").unwrap());
+    assert_eq!(&empty, res.0.get("the").unwrap());
+    assert_eq!(&empty, res.0.get("driven").unwrap());
+    assert_eq!(&empty, res.0.get("on").unwrap());
+
+    assert_ne!(&empty, res.0.get("car").unwrap());
+    assert_ne!(&empty, res.0.get("truck").unwrap());
+    assert_ne!(&empty, res.0.get("road").unwrap());
+    assert_ne!(&empty, res.0.get("highway").unwrap());
+}
+
+#[test]
+fn word_normalization_test() {
+    let sentences = HashMap::from_iter([
+        ("A", "foo foo, fo-o .foo FoO"),
+    ]);
+
+    let res = calc(&sentences);
+    let empty = HashMap::from_iter([
+        ("A", 0.0),
+    ]);
+    assert_eq!(res.0.len(), 1);
+    assert_eq!(&empty, res.0.get("foo").unwrap());
+}
