d46f9c1de7dfc89b3180ff32abde2fa454eb6bf7
kfigon <kamil.figon@gmail.com>
2023-11-04 00:30:03 +0100

    simple tcp chat on channels

--- chat-app/src/main.rs
+++ chat-app/src/main.rs
@@ -1,24 +1,31 @@
-use std::{net::{TcpListener, TcpStream}, thread, io::{Write, Read}, sync::{Mutex, Arc}};
+use std::{net::{TcpListener, TcpStream}, thread, io::{Write, Read}, sync::{Mutex, Arc, mpsc::{Receiver, Sender}}};
 
 const PORT: i32 = 8080;
 
-// send data with 
-// echo -n "foo" | nc localhost 8080
-
-// or continously with 
+// open con in multiple windows
 // nc localhost 8080
 
 fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind(format!("127.0.0.1:{PORT}"))?;
     println!("starting a server on {PORT}");
 
-    let history: Arc<Mutex<Vec<String>>> = Arc::new(Mutex::new(vec![]));
+    let (tx,rx): (Sender<String>, Receiver<String>)= std::sync::mpsc::channel();
+    let clients: Arc<Mutex<Vec<TcpStream>>> = Arc::new(Mutex::new(vec![]));
+
+    {
+        // server
+        let clients = clients.clone();
+        thread::spawn(move|| dispatch_msg(clients, rx));
+    }
 
     for stream in listener.incoming() {
         match stream {
             Ok(v) => {
-                let msgs = history.clone();
-                thread::spawn(move ||handle_client(v, msgs))
+                clients.lock().unwrap().push(v.try_clone().unwrap());
+                println!("client connected");
+
+                let sender = tx.clone();
+                thread::spawn(move ||handle_client(v, sender))
             },
             Err(err) => { 
                 println!("error connection {err:?}");
@@ -29,16 +36,35 @@ fn main() -> std::io::Result<()> {
     Ok(())
 }
 
-fn handle_client(mut stream: TcpStream, msgs: Arc<Mutex<Vec<String>>>) {
-    let mut buf = String::new();
-    if let Err(e) =  stream.read_to_string(&mut buf) {
-        println!("erro reading {e}");
-    }
-    
-    let mut d = msgs.lock().unwrap();
-    d.push(buf);
-    let d = d.join(", ");
-    if let Err(e) = stream.write(format!("[{d}]").as_bytes()) {
-        println!("error writing to stream: {e}");
+fn handle_client(mut stream: TcpStream, msgs: Sender<String>) {
+    loop {
+        let mut buf = [0;512];
+        if let Err(e) = stream.read(&mut buf) {
+            println!("erro reading {e}");
+            continue;
+        }
+        
+        msgs.send(String::from_utf8(buf.to_vec()).unwrap()).unwrap();
     }
 }
+
+fn dispatch_msg(clients: Arc<Mutex<Vec<TcpStream>>>, rx: Receiver<String>) {
+    loop {
+        let data = match rx.recv() {
+            Err(e) => {
+                println!("error receiving data {e}");
+                continue;
+            },
+            Ok(v) => v,
+        };
+        
+        {
+            let mut cs = clients.lock().unwrap();
+
+            for c in cs.iter_mut() {
+                c.write(format!("==== {data}").as_bytes()).unwrap();
+            }
+        }
+    }
+}
+
