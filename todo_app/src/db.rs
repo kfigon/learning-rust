d54072163a09eea170807714c340e5cac4bfdc7c
kfigon <kamil.figon@gmail.com>
2023-11-18 21:03:06 +0100

    axum hello

--- todo_app/src/db.rs
+++ todo_app/src/db.rs
@@ -1,4 +1,4 @@
-use postgres::{Client, NoTls, Row};
+use postgres::{Row};
 
 static USER: &'static str ="postgres";
 static PASSWORD: &'static str ="postgres";
@@ -20,24 +20,30 @@ impl TryFrom<&Row> for Person {
     }
 }
 
-#[test]
-fn connect_to_db() {
-    let mut client = Client::connect(&format!("host=localhost user={USER} password={PASSWORD}"), NoTls).expect("error connecting to db");
+#[cfg(test)]
+mod test {
+    use postgres::{Client, NoTls};
+    use super::*;
+    
+    #[test]
+    fn connect_to_db() {
+        let mut client = Client::connect(&format!("host=localhost user={USER} password={PASSWORD}"), NoTls).expect("error connecting to db");
 
-    client.execute("delete from task", &[]).expect("error truncating task");
-    client.execute("delete from person", &[]).expect("error truncating person");
+        client.execute("delete from task", &[]).expect("error truncating task");
+        client.execute("delete from person", &[]).expect("error truncating person");
 
-    client.execute("INSERT INTO person (name) VALUES ($1)", &[&"Ferris"]).expect("error inserting");
-    client.execute("INSERT INTO person (name) VALUES ($1)", &[&"Steve"]).expect("error inserting");
+        client.execute("INSERT INTO person (name) VALUES ($1)", &[&"Ferris"]).expect("error inserting");
+        client.execute("INSERT INTO person (name) VALUES ($1)", &[&"Steve"]).expect("error inserting");
 
-    let data = client.query("SELECT id,name FROM person order by id", &[])
-        .expect("error querying")
-        .iter()
-        .map(|row| row.try_into())
-        .collect::<Result<Vec<Person>,_>>()
-        .expect("error mapping rows");
+        let data = client.query("SELECT id,name FROM person order by id", &[])
+            .expect("error querying")
+            .iter()
+            .map(|row| row.try_into())
+            .collect::<Result<Vec<Person>,_>>()
+            .expect("error mapping rows");
 
-    assert_eq!(data.len(), 2);
-    assert_eq!(data[0].name, "Ferris".to_string());
-    assert_eq!(data[1].name, "Steve".to_string());
+        assert_eq!(data.len(), 2);
+        assert_eq!(data[0].name, "Ferris".to_string());
+        assert_eq!(data[1].name, "Steve".to_string());
+    }
 }
--- todo_app/src/main.rs
+++ todo_app/src/main.rs
@@ -1,5 +1,20 @@
 mod db;
 
-fn main() {
-    println!("Hello, world!");
+use axum::{response::Html,routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/", get(handler))
+        .route("/healthcheck", get(|| async { "ok" }));
+
+    println!("starting on port 3000");
+    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn handler() -> Html<&'static str> {
+    Html("<h1>Hello, World!</h1>")
 }
