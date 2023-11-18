5cf6b2cdb57b56376e17fe730c0de3355b9c2269
kfigon <kamil.figon@gmail.com>
2023-11-18 21:58:12 +0100

    hello http

--- todo_app/src/main.rs
+++ todo_app/src/main.rs
@@ -1,15 +1,21 @@
 mod db;
 
-use axum::{response::Html,routing::get, Router};
+use axum::{
+    response::{Html},
+    routing::get,
+    Router,
+};
 
 #[tokio::main]
 async fn main() {
+    const PORT: i32 = 3000;
+
     let app = Router::new()
         .route("/", get(handler))
         .route("/healthcheck", get(|| async { "ok" }));
 
-    println!("starting on port 3000");
-    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+    println!("starting on port {PORT}");
+    axum::Server::bind(&format!("0.0.0.0:{PORT}").parse().unwrap())
         .serve(app.into_make_service())
         .await
         .unwrap();
