07379838fbe6d96eb3052f67350ea8bda146511e
kfigon <kamil.figon@gmail.com>
2023-11-22 21:46:55 +0100

    if expressions

--- lisp/src/evaluator.rs
+++ lisp/src/evaluator.rs
@@ -3,6 +3,7 @@ use std::collections::HashMap;
 use crate::parser::{CompilerError, SExpression};
 
 struct Evaluator {
+    // todo: store as RefCell?
     env: Env
 }
 
@@ -33,6 +34,10 @@ impl Evaluator {
                     SExpression::Identifier(id) => {
                             if id == "+" {
                                 return self.plus(e);
+                            } else if id == "=" {
+                                return self.equal(e);
+                            } else if id == "if" {
+                                return self.if_expression(e);
                             }
                             return Err(CompilerError::UnknownSymbol(id.clone()));
                             // let func = self.env.env.get(id);
@@ -49,30 +54,78 @@ impl Evaluator {
         }
     }
 
+    fn eval_to_number(&mut self, e: &SExpression) -> Result<i32, CompilerError> {
+        match e {
+            SExpression::Number(n) => Ok(*n),
+            SExpression::Identifier(_) => todo!(),
+            _ => {
+                let res = self.eval_expr(e.clone())?;
+                match res {
+                    SExpression::Number(n) => Ok(n),
+                    SExpression::Identifier(_) => todo!(),
+                    _ => Err(CompilerError::InvalidList(res)),
+                }
+            }
+        }
+    }
+
+    fn eval_to_bool(&mut self, e: &SExpression) -> Result<bool, CompilerError> {
+        match e {
+            SExpression::Boolean(n) => Ok(*n),
+            SExpression::Identifier(_) => todo!(),
+            _ => {
+                let res = self.eval_expr(e.clone())?;
+                match res {
+                    SExpression::Boolean(n) => Ok(n),
+                    SExpression::Identifier(_) => todo!(),
+                    _ => Err(CompilerError::InvalidList(res)),
+                }
+            }
+        }
+    }
+
     fn plus(&mut self, e: SExpression) -> Result<SExpression, CompilerError> {
         match e {
             SExpression::List(v) => {
-                let args = &v[1..];
                 let mut out = 0;
-                for a in args {
-                    match a {
-                        SExpression::Number(n) => out += *n,
-                        _ => {
-                            let res = self.eval_expr(a.clone())?;
-                            match res {
-                                SExpression::Number(n) => out += n,
-                                _ => return Err(CompilerError::InvalidList(res)),
-                            }
-                        }
-                    }
+                for a in &v[1..] {
+                    let num = self.eval_to_number(&a)?;
+                    out += num;
                 }
                 return Ok(SExpression::Number(out));
             },
-            SExpression::Identifier(_) => Err(CompilerError::InvalidList(e)),
-            SExpression::Void => Err(CompilerError::InvalidList(e)),
-            SExpression::Number(_) => Err(CompilerError::InvalidList(e)),
-            SExpression::Boolean(_) => Err(CompilerError::InvalidList(e)),
-            SExpression::String(_) => Err(CompilerError::InvalidList(e)),
+            _ => Err(CompilerError::InvalidList(e)),
+        }
+    }
+
+    fn if_expression(&mut self, e: SExpression) -> Result<SExpression, CompilerError> {
+        match e {
+            SExpression::List(v) if v.len() == 4 => {
+                let condition = self.eval_to_bool(&v[1])?;
+                if condition {
+                    Ok(v[2].clone())
+                } else {
+                    Ok(v[3].clone())
+                }
+            },
+            _ => Err(CompilerError::InvalidList(e)),
+        }
+    }
+
+    fn equal(&mut self, e: SExpression) -> Result<SExpression, CompilerError> {
+        match e {
+            SExpression::List(ref v) if v.len() == 3 => {
+                let first = self.eval_expr(v[1].clone())?;
+                let second = self.eval_expr(v[2].clone())?;
+                match (first, second) {
+                    (SExpression::Number(a), SExpression::Number(b)) => Ok(SExpression::Boolean(a==b)),
+                    (SExpression::Boolean(a), SExpression::Boolean(b)) => Ok(SExpression::Boolean(a==b)),
+                    (SExpression::String(a), SExpression::String(b)) => Ok(SExpression::Boolean(a==b)),
+                    // todo: identifiers
+                    _ => Err(CompilerError::InvalidList(e.clone()))
+                }
+            },
+            _ => Err(CompilerError::InvalidList(e)),
         }
     }
 }
@@ -127,8 +180,24 @@ mod test {
     #[test]
     fn eval_plus_nested() {
         let r = run("(+ (+ 4 5) 2)");
-        assert_eq!(r, vec![
-            SExpression::Number(11),
-        ])
+        assert_eq!(r, vec![SExpression::Number(11)])
+    }
+
+    #[test]
+    fn eval_plus_nested2() {
+        let r = run("(+ (+ 4 (+ 3 2)) 2)");
+        assert_eq!(r, vec![SExpression::Number(11)])
+    }
+
+    #[test]
+    fn if_expression() {
+        let r = run(r#"(if (= 1 2) "ok" "not ok")"#);
+        assert_eq!(r, vec![SExpression::String("\"not ok\"".to_owned())])
+    }
+
+    #[test]
+    fn if_expression_2() {
+        let r = run(r#"(if (= (+ 1 1) 2) "ok" "not ok")"#);
+        assert_eq!(r, vec![SExpression::String("\"ok\"".to_owned())])
     }
 }
